258fd6cb902f3314809b89c6673763a6eeb6b8b1
Sergi Cortés Guerrero <sairaix@Sergis-MacBook-Air.local>
2023-01-26 13:52:20 +0100

    First Load

---
+++ take_the_register_now/android/app/src/main/kotlin/com/example/take_the_register_now/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.take_the_register_now
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ take_the_register_now/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import mobile_scanner
+import quick_blue_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  QuickBlueMacosPlugin.register(with: registry.registrar(forPlugin: "QuickBlueMacosPlugin"))
+}
