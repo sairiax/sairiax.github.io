ee245d3f2e9d710eae39c069e63eeeebf31dea04
Sergi Cortés Guerrero <sairaix@Sergis-MacBook-Air.local>
2023-01-26 14:06:35 +0100

    2nd Try

--- take_the_register_now/android/app/src/main/kotlin/com/example/take_the_register_now/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.take_the_register_now
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ take_the_register_pro/android/app/src/main/kotlin/com/example/take_the_register_pro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.take_the_register_pro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
