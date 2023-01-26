84445ce37a78a64a74940aa00f7be2b5bd182381
Sergi Cortés Guerrero <sairaix@Sergis-MacBook-Air.local>
2023-01-26 14:39:54 +0100

    things

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5ec3cb4b4ecfb8c1dead95b7a3f359f7",
-"index.html": "044b938e1738e1f0efb35190f630524b",
-"/": "044b938e1738e1f0efb35190f630524b",
-"main.dart.js": "939338bf718df780320d9dc48198afa1",
+"index.html": "8cb6c5e4c1da5feb5223180e557d0e4d",
+"/": "8cb6c5e4c1da5feb5223180e557d0e4d",
+"main.dart.js": "57fe37c257c1afc02d25bc1039364189",
+"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -17,7 +18,11 @@ const RESOURCES = {
 "assets/NOTICES": "259068529bed6e57beac74e9760f7147",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080"
+"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
 };
 
 // The application shell files that are downloaded before a service worker can
